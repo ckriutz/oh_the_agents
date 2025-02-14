c7059db2548ba99e33c6613fc902b2ed81e984c9
Casey Kriutzfield <casey.kriutzfield@microsoft.com>
2025-02-14 22:48:10 +0000

    Moved back to CrewAI

--- app.py
+++ app.py
@@ -1,36 +1,10 @@
-# This is going to a streamlit app that will use the AI agents to generate content.
-# Using Semantic Kernel and Azure OpenAI. Streamlit will be used to create the UI.
-# Semantic Kernel Agents will need to be able to search the web for clothing items.
-# The content will be prompted by the user telling us where they are going, and what time of the year.
-# From there our agents will look up the weather for that location, and then recommend what to wear.
-# The agents will also look up specific clothing items that are available for purchase, and present them to the user.
-# Lastly, the agents will display the cost of the clothing items, and the total cost.
-# We are going to have 5 agents
-# 1. Weather Agent
-# 2. Clothing Agent
-# 3. Shopping Agent
-# 4. Display Agent
-# 5. Cost Agent
-
 import asyncio
 import os
 import yaml
 import streamlit as st
-
-from semantic_kernel import Kernel
-from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion, AzureChatPromptExecutionSettings
-from semantic_kernel.functions import KernelArguments, KernelParameterMetadata, KernelPlugin
-from semantic_kernel.prompt_template import PromptTemplateConfig
-from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
-from semantic_kernel.agents import ChatCompletionAgent, AgentGroupChat
-from semantic_kernel.contents import ChatMessageContent, AuthorRole
-from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
-from semantic_kernel.contents.chat_history import ChatHistory
-from semantic_kernel.filters.filter_types import FilterTypes
-from semantic_kernel.connectors.search.bing import BingSearch
-from semantic_kernel.filters.functions.function_invocation_context import FunctionInvocationContext
-from semantic_kernel.connectors.search_engine import BingConnector, GoogleConnector
-from semantic_kernel.core_plugins import WebSearchEnginePlugin
+from crewai import Agent, Task, Crew, LLM
+from output_classes import SocialMediaPost, ContentOutput
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool, WebsiteSearchTool
 
 async def main():
     # First we start by creating the streamlit app.
@@ -43,36 +17,129 @@ async def main():
     azure_openai_endpoint = st.sidebar.text_input('Azure OpenAI Endpoint', '')
     azure_openai_version = st.sidebar.text_input('Azure OpenAI Version', '2024-05-01-preview')
     azure_openai_deployment_name = st.sidebar.text_input('Azure OpenAI Deployment Name', 'gpt-4o')
-    bing_api_key = st.sidebar.text_input('Bing API Key', '')
+    serper_api_key = st.sidebar.text_input('Serper API Key', '')
     
-    # Define the Kernel
-    kernel = Kernel()
+    # Required
+    os.environ['AZURE_OPENAI_API_KEY'] = azure_openai_key
+    os.environ['AZURE_OPENAI_ENDPOINT'] = azure_openai_endpoint
+    os.environ['AZURE_API_VERSION'] = azure_openai_version
+    os.environ['SERPER_API_KEY'] = serper_api_key
 
-    # Add Azure OpenAI Connector
-    chat_completion_service = AzureChatCompletion(
-        deployment_name=azure_openai_deployment_name,  
-        api_key=azure_openai_key,
-        endpoint=azure_openai_endpoint, # Used to point to your service
-        api_version=azure_openai_version, # Used to point to your service
+    # Define file paths for YAML configurations
+    files = {
+        'agents': 'config/agents.yaml',
+        'tasks': 'config/tasks.yaml'
+    }
+
+    # Load configurations from YAML files
+    configs = {}
+    for config_type, file_path in files.items():
+        with open(file_path, 'r') as file:
+            configs[config_type] = yaml.safe_load(file)
+
+    # Assign loaded configurations to specific variables
+    agents_config = configs['agents']
+    tasks_config = configs['tasks']
+
+    websearchtool = WebsiteSearchTool(
+        config=dict(
+            llm=dict(
+                provider="azure_openai", # or google, openai, anthropic, llama2, ...
+                config=dict(
+                    model="gpt-4o-mini",
+                    # temperature=0.5,
+                    # top_p=1,
+                    # stream=true,
+                ),
+            ),
+            embedder=dict(
+                provider="azure_openai", # or openai, ollama, ...
+                config=dict(
+                    model="text-embedding-ada-002",
+                    deployment_name= "text-embedding-ada-002",
+                    #task_type="retrieval_document",
+                    # title="Embeddings",
+                ),
+            ),
+        )
     )
 
-    connector = BingConnector(bing_api_key)
-    kernel.add_plugin(WebSearchEnginePlugin(connector), "WebSerarch")
-    kernel.add_service(chat_completion_service)
-
-    arguments = KernelArguments(
-        settings=PromptExecutionSettings(function_choice_behavior=FunctionChoiceBehavior.Auto(),)
+    # Creating Agents
+    market_news_monitor_agent = Agent(
+        config=agents_config['market_news_monitor_agent'],
+        tools=[SerperDevTool(), ScrapeWebsiteTool()],
+        llm=LLM(model=f'azure/gpt-4o'),
     )
+
+    data_analyst_agent = Agent(
+        config=agents_config['data_analyst_agent'],
+        tools=[SerperDevTool(), websearchtool],
+        llm=LLM(model=f'azure/gpt-4o'),
+    )
+
+    content_creator_agent = Agent(
+        config=agents_config['content_creator_agent'],
+        tools=[SerperDevTool(), websearchtool], # had to customize WebsiteSearchTool
+        llm=LLM(model=f'azure/gpt-4o')
+    )
+
+    quality_assurance_agent = Agent(
+        config=agents_config['quality_assurance_agent'],
+        llm=LLM(model=f'azure/gpt-4o')
+    )
+
+    # Creating Tasks
+    monitor_financial_news_task = Task(
+        config=tasks_config['monitor_financial_news'],
+        agent=market_news_monitor_agent
+    )
+
+    analyze_market_data_task = Task(
+        config=tasks_config['analyze_market_data'],
+        agent=data_analyst_agent
+    )
+
+    create_content_task = Task(
+        config=tasks_config['create_content'],
+        agent=content_creator_agent,
+        context=[monitor_financial_news_task, analyze_market_data_task]
+    )
+
+    quality_assurance_task = Task(
+        config=tasks_config['quality_assurance'],
+        agent=quality_assurance_agent,
+        output_pydantic=ContentOutput
+    )
+
+    # Creating Crew
+    content_creation_crew = Crew(
+        agents=[
+            market_news_monitor_agent,
+            data_analyst_agent,
+            content_creator_agent,
+            quality_assurance_agent
+        ],
+        tasks=[
+            monitor_financial_news_task,
+            analyze_market_data_task,
+            create_content_task,
+            quality_assurance_task
+        ],
+        verbose=True
+    )
+
+    # Assign loaded configurations to specific variables
+    agents_config = configs['agents']
+    tasks_config = configs['tasks']
 
     # Now we create the form, which will contain the text input, as well as the submit button.
     with st.form('ai_form'):
-        text = st.text_area('Subject:', 'What is the recommendation for what to wear in Seattle in the spring for an adult male?')
+        text = st.text_area('Subject:', 'The effect of inflation on the markets in 2024.')
 
         submit = st.form_submit_button('Submit')
         if submit:
-            response = await kernel.invoke_prompt(text, arguments=arguments)
-            #result = await kernel.invoke(web_plugin["search"], query=text)
-            st.write(response.value)
+            result = content_creation_crew.kickoff(inputs={'subject': text})
+            st.write(result)
             
     
 if __name__ == "__main__":
